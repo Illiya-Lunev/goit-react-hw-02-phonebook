3c92444d48451243961be3d5c106273be80d8282
Iliya Lunev <lunevig90@gmail.com>
2021-10-07 17:04:00 +0300

    Проверка

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
+}

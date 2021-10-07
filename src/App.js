5a6dd45402ff1a021aeddf8385bf519c6456dfe4
Iliya Lunev <lunevig90@gmail.com>
2021-10-07 18:34:30 +0300

    добавлен рендер Phonebook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Component } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+// import { v4 as uuidv4 } from 'uuid';
+import FormPhoneBook from './FormPhoneBook/FormPhoneBook';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>Phonebook</h1>
+        <FormPhoneBook />
         <h2>Contacts</h2>
       </div>
     );
---
+++ src/FormPhoneBook/FormPhoneBook.js
@@ -0,0 +1,65 @@
+import { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+export default class FormPhoneBook extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  nameInputId = uuidv4();
+  numberInputId = uuidv4();
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    const { name, number } = e.currentTarget;
+    e.preventDefault();
+    this.props.onSubmit(name, number);
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor={this.nameInputId}>
+          Name
+          <input
+            type="text"
+            name="name"
+            id={this.nameInputId}
+            value={this.state.name}
+            onChange={this.handleChange}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Имя может состоять только из букв, апострофа, тире и
+            пробелов. Например Adrian, Jacob Mercer, Charles de Batz de
+            Castelmore d'Artagnan и т. п."
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <label htmlFor={this.numberInputId}>
+          Number
+          <input
+            type="tel"
+            name="number"
+            id={this.numberInputId}
+            value={this.state.number}
+            onChange={this.handleChange}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+            required
+            onChange={this.handleChange}
+          />
+        </label>
+        <button type="submit">Add to contacts</button>
+      </form>
+    );
+  }
+}
